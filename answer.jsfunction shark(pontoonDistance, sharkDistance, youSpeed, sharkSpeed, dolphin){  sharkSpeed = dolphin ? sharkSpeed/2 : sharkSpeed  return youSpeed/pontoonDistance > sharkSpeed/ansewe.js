8720fb2c851fdb999188daa5b477a4b23b2768fb
mnubhan <76425959+mnubhan@users.noreply.github.com>
2024-01-20 19:44:37 +0800

    Delete answer.jsfunction shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){  sharkSpeed = dolphin ? sharkSpeed/2 : sharkSpeed  return youSpeed/pontoonDistance > sharkSpeed directory

--- answer.jsfunction shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){  sharkSpeed = dolphin ? sharkSpeed/2 : sharkSpeed  return youSpeed/pontoonDistance > sharkSpeed/ansewe.js
+++
@@ -1,4 +0,0 @@
-function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
- sharkSpeed = dolphin ? sharkSpeed/2 : sharkSpeed
- return youSpeed/pontoonDistance > sharkSpeed/sharkDistance ? "Alive!": "Shark Bait!"
-}
